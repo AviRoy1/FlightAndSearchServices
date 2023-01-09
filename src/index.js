fa07683c655ae706edc15e21a0c57171ad02a590
avijit <avijitroy2944@gmail.com>
2023-01-09 11:44:26 +0530

    Add city controller

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const { PORT } = require('./config/serverConfig');
+const Cityrepository = require('./repository/city-repository');
 
 const setupAndStartServer = async ()=>{
     const app = express();
-    app.listen(PORT , ()=>{
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.listen(PORT , async ()=>{
         console.log(`Server started at ${PORT}`);
        // console.log(process.env)
     })
